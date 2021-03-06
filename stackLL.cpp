40bc6b981cfc05cc94dc277b5e70717539904838
saurabhchopade <saurabhchopade3250@gmail.com>
2021-03-06 17:40:08 +0530

    ADDED: Implemented stack using linkedlist

---
+++ stackLL.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+template<typename T>
+class Node{
+    public:
+        T data;
+        Node<T> *next;
+    
+    Node(){
+    }
+    Node(T data){
+        this->data = data;
+        next = NULL;
+    }
+};
+
+template<typename T>
+class stackLL : public Node<T>{
+public:
+
+    Node<T> *head;
+    int size = 0;
+    stackLL(){
+        head = NULL;
+    }
+
+    void push(T element){
+        if(head == NULL){
+            size++;
+            head = new Node<T>(element);
+            return;
+        }
+        size++;
+        Node<T> *newNode = new Node<T>(element);
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    void top(){
+        if(head == NULL){
+            cout << "Stack Is Empty" << endl;
+            return;
+        }
+            cout <<"top = " << head->data << endl;
+    }
+
+    void pop(){
+        if(head == NULL){
+            cout << "Invalid Operation === Stack is Empty"<< endl;
+            return;
+        }  
+
+        size--;
+        Node<T> *dummy = head;
+        head = head->next;
+    }
+
+    void sizeOfStack(){
+        cout<< "Size of the stack is= "<< size << endl;
+    }
+};
+
+int main() {
+stackLL<string> obj;
+obj.push("saurabh");
+obj.top();
+obj.pop();
+obj.sizeOfStack();
+}
